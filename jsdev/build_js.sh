22df38aa725c0e6e4fe88ef3175d81e2d1b00f35
vbtc-exchange <vbtc-exchange@users.noreply.github.com>
2022-12-08 22:28:50 +0700

    Change to compile with recent version of the Closure compiler

--- jsdev/build_js.sh
+++ jsdev/build_js.sh
@@ -1,48 +1,111 @@
 #!/bin/bash
 
 #############################################
-# remove compiled js
-#############################################
-#rm -rf ./bitex/templates/*.soy.js
-
-#############################################
 # set defaults if they're not provided
 #############################################
 LANGUAGE=${LANGUAGE:=en_US}
 THEME=${THEME:=default}
 
+
+#############################################
+# compile project
+#############################################
+
+OUTPUT_FILE=../assets/js/bitex_app_blink_trade.compiled.$LANGUAGE.$THEME.js
+
+COMPILER_JAR_EXEC=../node_modules/.bin/google-closure-compiler
+if [ -f "$COMPILER_JAR_EXEC" ]; then
+  echo "Found compiler at $COMPILER_JAR_EXEC"
+else
+  COMPILER_JAR="../node_modules/google-closure-compiler/compiler.jar"
+  if [ -f "$COMPILER_JAR" ]; then
+    echo "Found compiler at $COMPILER_JAR"
+    COMPILER_JAR_EXEC="java -jar $COMPILER_JAR"
+  else
+    echo "ERROR: Could not find compiler, exiting"
+    exit
+  fi
+fi
+
+
+echo "Compiling/Google Closure"
+echo "  output_file: $OUTPUT_FILE"
+
 # BlinkTrade Application
-python2 ./closure-library/closure/bin/build/closurebuilder.py  \
-  --root=./closure-library/ \
-  --root=./closure-bootstrap/javascript/ \
-  --root=./scottlogic/ \
-  --root=./uniform/ \
-  --root=./libphonenumber/i18n/phonenumbers/ \
-  --root=./expression_evaluator/ \
-  --root=./bitex \
-  --namespace=bitex.app.BlinkTrade \
-  --output_mode=compiled \
-  --compiler_jar=./tools/compiler.jar \
-  --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
-  --compiler_flags="--define=goog.DEBUG=0" \
-  --compiler_flags="--define=goog.LOCALE='$LANGUAGE'" \
-  --compiler_flags="--externs=./externs/google.js" \
-  --compiler_flags="--externs=./externs/plusone.js" \
-  --compiler_flags="--externs=./externs/zopim.js" \
-  --compiler_flags="--externs=./externs/jquery-1.9.js" \
-  --compiler_flags="--externs=./externs/twitter-bootstrap.js" \
-  --compiler_flags="--externs=./externs/google_analytics_api.js" \
-  --compiler_flags="--externs=./externs/parsley.js" \
-  --compiler_flags="--externs=./externs/jquerymobile-1.4.3.js" \
-  --compiler_flags="--externs=./externs/sticky.js" \
-  --compiler_flags="--externs=./externs/socket.io.js" \
-  --compiler_flags="--externs=./externs/w3c_rtc.js" \
-  --compiler_flags="--externs=./externs/facebook_javascript_sdk.js" \
-  --compiler_flags="--externs=./externs/qrcode.js" \
-  --compiler_flags="--translations_file=./translations/$LANGUAGE.xtb.xml" \
-   > ../assets/js/bitex_app_blink_trade.compiled.$LANGUAGE.$THEME.js
+$COMPILER_JAR_EXEC \
+  --js_output_file $OUTPUT_FILE \
+  --js ../node_modules/google-closure-library/closure/goog/**.js \
+  --js ../node_modules/google-closure-library/third_party/closure/goog/**.js \
+  --js ./scottlogic/**.js \
+  --js ./uniform/**.js \
+  --js ./libphonenumber/i18n/phonenumbers/**.js \
+  --js ./expression_evaluator/**.js \
+  --js ./bitex/**.js \
+  --js ./closure-bootstrap/javascript/**.js \
+  --externs ./externs/google.js \
+  --externs ./externs/plusone.js \
+  --externs ./externs/zopim.js \
+  --externs ./externs/jquery-1.9.js \
+  --externs ./externs/twitter-bootstrap.js \
+  --externs ./externs/google_analytics_api.js \
+  --externs ./externs/parsley.js \
+  --externs ./externs/jquerymobile-1.4.3.js \
+  --externs ./externs/sticky.js \
+  --externs ./externs/socket.io.js \
+  --externs ./externs/facebook_javascript_sdk.js \
+  --externs ./externs/qrcode.js \
+  --define goog.DEBUG=0 \
+  --define goog.LOCALE="$LANG" \
+  --debug \
+  --formatting=PRETTY_PRINT \
+  --formatting=PRINT_INPUT_DELIMITER \
+  --compilation_level ADVANCED_OPTIMIZATIONS
+
+
+
+# Removed
+#
+#  --entry_point ./bitex/app/blinktrade \
+#  not working, not a valid option
+#  --translations_file ./translations/$LANGUAGE.xtb.xml \
+#  libraries:
+#  --externs ./externs/w3c_rtc.js \
 
 # enable those flags to debug the compiled code.
-#  --compiler_flags="--debug=TRUE" \
-#  --compiler_flags="--formatting=PRETTY_PRINT" \
-#  --compiler_flags="--formatting=PRINT_INPUT_DELIMITER" \
+#  --debug \
+#  --formatting=PRETTY_PRINT \
+#  --formatting=PRINT_INPUT_DELIMITER \
+
+
+# inspiration, from compilation of libphonenumber
+#
+#${closure-compiler.jar}
+#--output_wrapper=@{outputwrapper}
+#--compilation_level=@{compilationlevel}
+#--warning_level=VERBOSE
+#--jscomp_error=accessControls
+#--jscomp_error=checkDebuggerStatement
+#--jscomp_error=checkRegExp
+#--jscomp_error=checkTypes
+#--jscomp_error=checkVars
+#--jscomp_error=const
+#--jscomp_error=constantProperty
+#--jscomp_error=duplicate
+#--jscomp_error=duplicateMessage
+#--jscomp_error=es5Strict
+#--jscomp_error=externsValidation
+#--jscomp_error=extraRequire
+#--jscomp_error=globalThis
+#--jscomp_error=invalidCasts
+#--jscomp_error=misplacedTypeAnnotation
+#--jscomp_error=missingProperties
+#--jscomp_error=missingRequire
+#--jscomp_error=nonStandardJsDocs
+#--jscomp_error=strictModuleDepCheck
+#--jscomp_error=suspiciousCode
+#--jscomp_error=typeInvalidation
+#--jscomp_error=undefinedNames
+#--jscomp_error=undefinedVars
+#--jscomp_error=unknownDefines
+#--jscomp_error=uselessCode
+#--jscomp_error=visibility
