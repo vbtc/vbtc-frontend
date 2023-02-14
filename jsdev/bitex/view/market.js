cad7f468e6cfd4b41f014fa39add61d2bb80029f
vbtc-exchange <vbtc-exchange@users.noreply.github.com>
2023-02-15 02:23:25 +0700

    Missing semicolon in market.js

--- jsdev/bitex/view/market.js
+++ jsdev/bitex/view/market.js
@@ -6,7 +6,7 @@ goog.require('bitex.ui.MarketViewTable.EventType');
 goog.require('bitex.util');
 
 goog.require('bitex.templates');
-goog.require('bitex.util')
+goog.require('bitex.util');
 goog.require('bitex.ui.SimpleChart');
 
 /**
@@ -177,7 +177,7 @@ bitex.view.MarketView.prototype.onTradeHistoryTableRequestData_ = function(e) {
   var page = e.options['Page'];
   var limit = e.options['Limit'];
   var filter = e.options['Filter'];
-  
+
   var conn = this.getApplication().getBitexConnection();
   conn.requestTradeHistory(this.market_data_subscription_id_, page, limit, undefined, filter );
 };
